940ea5db787dbcc3932978e7ef6708c5af4ebd71
Curry5004 <yunjin5004@naver.com>
2022-01-02 22:04:50 +0900

    codetest 01-02 solved

---
+++ cosmo/CodeTest/Java/Chap01/src/Ex02/Main.java
@@ -0,0 +1,23 @@
+package Ex02;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String input1 = in.nextLine();
+    
+
+  }
+  public static String solution(String str1) {
+	  String str = str1;
+	  char[] charArr = new char[str.length()];
+
+	  for(int i=0; i<str.length(); i++) {
+		  charArr[i] = str.charAt(i);
+     }
+	  for(int i=0; i<str.length(); i++) {
+		  if(charArr[i]z`)
+	  }
+  }
+}
