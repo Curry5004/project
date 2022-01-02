9615547539197cf59a075743ec924fd2c2b6bcf5
Curry5004 <yunjin5004@naver.com>
2022-01-02 21:49:24 +0900

    code test 01-01 solved

--- cosmo/CodeTest/Java/Chap01/src/Ex01/Main.java
+++ cosmo/CodeTest/Java/Chap01/src/Ex01/Main.java
@@ -8,19 +8,26 @@ public class Main {
     Scanner in=new Scanner(System.in);
     String input1 = in.nextLine();
     String input2 = in.nextLine();
-    System.out.println(input1 + input2);
-    solution(input1, input2);
-    return ;
+    System.out.println(solution(input1, input2)); 
   }
-  public static char solution(String input1, String input2) {
+  public static int solution(String input1, String input2) {
      String str = input1.toLowerCase();
      String char1 = input2.toLowerCase();
+     char char2 = char1.charAt(0);
+     int counter = 0;
+   
+     char[] strarr = new char[str.length()];
+     for(int i=0; i<str.length(); i++) {
+    	 strarr[i] = str.charAt(i);
+     }
      
-     
-     //충충돌 테스트
-    int search =str.indexOf(char1);
-    System.out.println("hi");
-    return 'a';
+     for(int i=0; i<strarr.length; i++) {
+    	 if(strarr[i]==char2) {
+    		 counter +=1;
+    	 }
+     }
+    
+    return counter;
   
   }
 }
