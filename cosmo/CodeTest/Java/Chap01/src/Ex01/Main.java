a0aab17ee2afd3619fc3cf771b05f2b0862428ab
Curry5004 <yunjin5004@naver.com>
2022-01-02 17:41:47 +0900

    codetest add

---
+++ cosmo/CodeTest/Java/Chap01/src/Ex01/Main.java
@@ -0,0 +1,22 @@
+package Ex01;
+import java.util.Scanner;
+
+public class Main {
+    
+   
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String input1 = in.nextLine();
+    String input2 = in.nextLine();
+    System.out.println(input1 + input2);
+    solution(input1, input2);
+    return ;
+  }
+  public static char solution(String input1, String input2) {
+     String str = input1.toLowerCase();
+     String char1 = input2.toLowerCase();
+     
+    int search =str.indexOf(char1);
+    return 'a';
+  }
+}
