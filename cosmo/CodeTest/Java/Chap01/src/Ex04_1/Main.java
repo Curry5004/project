53adc226362956ae74552b824347db4cc0ff6980
Curry5004 <yunjin5004@naver.com>
2022-01-04 10:28:31 +0900

    codetest 01-04 해결

--- cosmo/CodeTest/Java/Chap01/src/Ex04_1/Main.java
+++ cosmo/CodeTest/Java/Chap01/src/Ex04_1/Main.java
@@ -8,18 +8,32 @@ public class Main {
 		Main t = new Main();
 		Scanner in = new Scanner(System.in);
 		int input1 = in.nextInt();
-		t.solution(input1);
+		String[] strArr = new String[input1];
+		for(int i=0; i<strArr.length; i++) {
+			strArr[i]=in.next();
+		}
+		t.solution(strArr);
+
+		
+		
+		
 	}
-	public void solution(int input) {
-		String[] strArr = new String[input];
-		for(int i=0; i<strArr.length; i++) {
-			Scanner in = new Scanner(System.in);
-			strArr[i]=in.nextLine();
-		}
-		for(int i=0; i<strArr.length; i++) {
-			char[] charArr= new char[strArr[i].length()];
-			
-			
-		}
+	public void solution(String[] strArr) {				
+		boolean run= true;
+		for(int i=0; i<strArr.length;i++) {
+			String c = "";
+			run=true;
+			while(run) {
+				char a = strArr[i].charAt(strArr[i].length()-1);
+				c += a;
+				if(strArr[i].length()!=1) {
+					strArr[i] = strArr[i].substring(0,strArr[i].length()-1);
+				} else {
+					run=false;
+				}
+				
+			}
+			System.out.println(c);
+		}		
 	}
 }
