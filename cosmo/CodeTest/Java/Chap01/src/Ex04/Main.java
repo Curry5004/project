214d9e163ec8acec835c253ecb02afe52e42ce30
Curry5004 <yunjin5004@naver.com>
2022-01-04 10:55:31 +0900

    codetest 01-04 solved

--- cosmo/CodeTest/Java/Chap01/src/Ex04/Main.java
+++ cosmo/CodeTest/Java/Chap01/src/Ex04/Main.java
@@ -3,24 +3,22 @@ package Ex04;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Main t = new Main();
 		Scanner in = new Scanner(System.in);
 		int input1 = in.nextInt();
-		t.solution(input1);
-
-		
-		
-		
-	}
-	public void solution(int input) {
-		String[] strArr = new String[input];
-		
+		String[] strArr = new String[input1];
 		for(int i=0; i<strArr.length; i++) {
-			Scanner in = new Scanner(System.in);
-			strArr[i]=in.nextLine();
+			strArr[i]=in.next();
 		}
+		t.solution(strArr);
+
+
+
+
+	}
+	public void solution(String[] strArr) {				
 		boolean run= true;
 		for(int i=0; i<strArr.length;i++) {
 			String c = "";
@@ -33,9 +31,9 @@ public class Main {
 				} else {
 					run=false;
 				}
-				
+
 			}
 			System.out.println(c);
 		}		
 	}
-}
+} 
