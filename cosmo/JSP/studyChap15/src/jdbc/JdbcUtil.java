a8a8164d16c6f4e73ed037a287462701c49104b3
Curry5004 <yunjin5004@naver.com>
2022-01-05 17:01:24 +0900

    220105 수업내용

---
+++ cosmo/JSP/studyChap15/src/jdbc/JdbcUtil.java
@@ -0,0 +1,38 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcUtil {
+	public static void close(ResultSet rs) {
+		if(rs!=null) {
+			try {
+				rs.close();
+			} catch(SQLException ex) {}
+		}
+	}
+	public static void close(Statement rs) {
+		if(rs!=null) {
+			try {
+				rs.close();
+			} catch(SQLException ex) {}
+		}
+	}
+	public static void close(Connection conn) {
+		if(conn!=null) {
+			try {
+				conn.close();
+			} catch(SQLException ex) {}
+		}
+	}
+	public static void rollback(Connection conn) {
+		if(conn!=null) {
+			try {
+				conn.rollback();
+			} catch(SQLException ex) {}
+		}
+	}
+
+}
