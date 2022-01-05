1214a4861eda0e2d4c7593c7ae31e55e1d4ea416
Curry5004 <yunjin5004@naver.com>
2022-01-05 12:38:51 +0900

    codetest 01-05 푸는 중. 런타임 에러 발생

--- cosmo/CodeTest/Java/Chap01/src/Ex05/Main.java
+++ cosmo/CodeTest/Java/Chap01/src/Ex05/Main.java
@@ -5,14 +5,27 @@ import java.util.Scanner;
 public class Main {
   public String solution(String str) {
 	  char[] charArr= str.toCharArray();
+	  char[] ansArr= new char[str.length()];
 	  for(int i=0; i<charArr.length; i++) {
-		  if(65<=charArr[i] && charArr[i]<=90 && 97<=charArr[i] && charArr[i]<=122) {
-			  int count = charArr.length;
+		  if(!(65<=charArr[i] && charArr[i]<=90) && !(97<=charArr[i] && charArr[i]<=122)) {
+			  ansArr[i]=charArr[i];
 			  
 		  }
 	  }
 	  
-	  
+	  int len = str.length()-1;
+	  for(int i=0; i<str.length(); i++) {
+		  while(ansArr[len]!='\u0000') {		  
+			  len = len-1;
+		  }
+		  if((65<=charArr[i] && charArr[i]<=90) || (97<=charArr[i] && charArr[i]<=122)) {
+			  ansArr[len]=charArr[i];
+			}
+	  }
+	  String answer = new String(ansArr);
+	  return answer;
+			
+
   }
 	public static void main(String[] args){
 	 Main t = new Main();
